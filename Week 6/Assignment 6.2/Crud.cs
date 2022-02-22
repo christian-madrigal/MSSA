1722a9ac4d4bbfdca88ce1b64251044eabb53f9e
christian-madrigal <98770177+christian-madrigal@users.noreply.github.com>
2022-02-22 15:31:54 -0800

    Add files via upload

---
+++ Week 6/Assignment 6.2/Crud.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6._2
+{
+    interface Crud
+    {
+        ICollection<Car> ReadCars();
+        string GetMaxID();
+        void AddRecord(Car obj);
+        void DeleteRecord(Car obj);
+        ICollection<Car> GetCars();
+        Car FindCar(string id);
+        void UpdateRecord(string id, Car carchanges);
+    }
+
+    class CarRepository : Crud
+    {
+        CarInventoryEntities entities;
+
+        public CarRepository()
+        {
+            entities = new CarInventoryEntities();
+        }
+
+        public void AddRecord(Car obj)
+        {
+            entities.Cars.Add(obj);
+            entities.SaveChanges();
+        }
+        
+        public void DeleteRecord(Car obj)
+        {
+            entities.Cars.Remove(obj);
+            entities.SaveChanges();
+
+        }
+
+        public Car FindCar(string id)
+        {
+            var car = entities.Cars.First(n => n.Vin == id);
+            if (car != null)
+            {
+                return car;
+            }
+            else
+                return null;
+        }
+
+        public ICollection<Car> GetCars()
+        {
+            return entities.Cars.ToList();
+        }
+
+        public string GetMaxID()
+        {
+            return entities.Cars.Max(p => p.Vin);
+        }
+
+        public ICollection<Car> ReadCars()
+        {
+            return entities.Cars.ToList();
+        }
+
+        public void UpdateRecord(string id, Car carchanges)
+        {
+            var cartoupdate = entities.Cars.Find(id);
+            cartoupdate.Vin = carchanges.Vin;
+            cartoupdate.Make=carchanges.Make;
+            cartoupdate.Model=carchanges.Model;
+            cartoupdate.Color=carchanges.Color;
+            cartoupdate.Year=carchanges.Year;
+            cartoupdate.Price=carchanges.Price;
+            cartoupdate.Status = carchanges.Status;
+            entities.SaveChanges();
+
+        }
+
+    }
+}
---
+++ Week 6/Assignment 6.2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_6._2
+{
+    public partial class Form1 : Form
+    {
+        int attempt = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            txtPass.PasswordChar = '*';
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
+            if (attempt < 4)
+            {
+                string username = txtUser.Text;
+                string password = txtPass.Text;
+                if ((this.txtUser.Text == "Admin") && (this.txtPass.Text == "Password"))
+                {
+
+                    (new Form2()).Show(); this.Hide();
+                }
+                else 
+                {
+                    txtPass.Clear();
+                    txtUser.Clear();
+                    MessageBox.Show($"You have entered the wrong Username and/or Password. Please check to ensure you are using case sensitive letters.This is {attempt} of 3 attempts");
+
+                }
+            }
+            else if (attempt == 4)
+            {
+                MessageBox.Show("You have exceeded the of tries and you do not have access to use this system");
+
+
+
+            }
+            attempt++;
+           
+            
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
