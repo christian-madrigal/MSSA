b8273bf6933d1010c3820833c94c9237c038acca
christian-madrigal <98770177+christian-madrigal@users.noreply.github.com>
2022-02-22 15:20:25 -0800

    Add files via upload

---
+++ Week 2/Assignment 2.1 Part 2/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._1_Part_2
+{
+    public class Automotive
+    {
+        public string AutomotiveType { get; set; }
+    }
+
+    public class Car: Automotive
+    {
+        public string CarMake { get; set; }
+
+        public string BodyType { get; set; }
+    }
+
+    public class Honda:Car
+    {
+        public string Color { get; set; }
+
+        public string HorsePower { get; set; }
+    }
+
+    public class Cheverolete:Car
+    {
+        public string Color { get; set; }
+
+        public int HorsePower { get; set; }
+    }
+}
---
+++ Week 2/Assignment 2.1 Part 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._1_Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
