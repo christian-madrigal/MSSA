4bc21035899f36aef92689e7d2ee548ca544b5f8
christian-madrigal <98770177+christian-madrigal@users.noreply.github.com>
2022-03-21 02:09:24 -0700

    Assignment 10.3

---
+++ Week 10/Assingment 10.3/scripts/DOMScript.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('position').addEventListener('click', showPosition)
+});
+
+function showPosition() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let positioninfo = "Your current location is (" + "Latitude:" + position.coords.latitude + " Longitude :" + position.coords.longitude + ")";
+            document.getElementById("position").innerHTML = positioninfo;
+        });
+    }
+    else { alert("No geolocation data avilable"); }
+}
