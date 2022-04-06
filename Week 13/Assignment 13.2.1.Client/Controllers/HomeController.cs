bcf23059cf82f56d0507083f7326211181d6e9b4
christian-madrigal <98770177+christian-madrigal@users.noreply.github.com>
2022-04-06 08:07:56 -0700

    update

---
+++ Week 13/Assignment 13.2.1.Client/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Assignment_13._2._1.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+namespace Assignment_13._2._1.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IHttpClientFactory _httpClientFactory;
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+
+        }
+
+        public IActionResult Index()
+        {
+            HttpClient httpClient=_httpClientFactory.CreateClient();
+            httpClient.BaseAddress = new Uri("http://localhost:24175/");
+            var response = httpClient.GetStringAsync("http://localhost:24175/api/Product");
+            //return Content(response.Result);
+            return View();
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 13/Assignment 13.2.1/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Assignment_13._2._1.Models;
+namespace Assignment_13._2._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private List<Product> product = new List<Product>();
+        public ProductController()
+        {
+            product.Add(new Product() 
+            { 
+                Id = 101, 
+                Name = "Jordan Retro 11", 
+                Description = "The Air Jordan 11 Cool Grey (2021) features a Cool Grey Durabuck upper with patent leather overlays and a signature Jumpman embroidery on the collar. ", 
+                Price = 190, });
+            product.Add (new Product() 
+            { 
+                Id = 102, 
+                Name = "Yeezy Wave Runners", 
+                Description = "The Yeezy 700 Boost Wave Runner is the shoe that led Yeezy's pivot from a sleek, minimal aesthetic to a chunky, 90s inspired one. ", 
+                Price = 300, });
+            product.Add(new Product() 
+            { 
+                Id = 103, 
+                Name = "Yeezy Ochre Foam RNNR", 
+                Description = "The adidas Yeezy Foam RNNR Ochre features a golden Ochre one-piece EVA foam construction. Oval cut-outs throughout the design provide ventilation, while a cushioned footbed offers support. ", 
+                Price = 90, });
+            product.Add(new Product() 
+            { 
+                Id = 104, 
+                Name = "Jordan Retro 4 Oreos", 
+                Description = "The Air Jordan 4 Retro White Oreo features a white leather and mesh upper with hits of Tech Grey on its eyelets and midsole. From there, a red Jumpman logo is embroidered on the tongue, adding a pop of color to the neutral-toned design. ", 
+                Price = 190, });
+        }
+        [HttpGet()]
+        public List<Product> GetProducts()
+        {
+            return this.product;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProductbyId(int id)
+        {
+            var product = this.product.Find(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
