507a83a33843c228f261b7d239b7f6ffd2c1575b
christian-madrigal <98770177+christian-madrigal@users.noreply.github.com>
2022-04-11 02:08:59 -0700

    Update

---
+++ Week 13/Assignment 13.1/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_13._1.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
